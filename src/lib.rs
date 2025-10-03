d2a0c37ef69e77a8c291f4ae308f16c5a8024bba
Tim Van Wassenhove <github@timvw.be>
2025-10-03 22:10:47 +0200

    chore: run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -193,8 +193,8 @@ pub use traces::{IdGenerator, TraceResponse};
 // - Utility types: ScoreDataType
 pub use langfuse_client_base::models::{
     CreateEventBody, CreateGenerationBody, CreateSpanBody, Dataset, DatasetItem,
-    DatasetRunWithItems, IngestionBatchRequest, IngestionEvent, ObservationLevel,
-    ObservationsView, ObservationsViews, PaginatedDatasetItems, PaginatedDatasetRuns,
-    PaginatedDatasets, Prompt, PromptMetaListResponse, ScoreDataType, Trace, TraceBody,
-    TraceWithDetails, TraceWithFullDetails, Traces,
+    DatasetRunWithItems, IngestionBatchRequest, IngestionEvent, ObservationLevel, ObservationsView,
+    ObservationsViews, PaginatedDatasetItems, PaginatedDatasetRuns, PaginatedDatasets, Prompt,
+    PromptMetaListResponse, ScoreDataType, Trace, TraceBody, TraceWithDetails,
+    TraceWithFullDetails, Traces,
 };
